75253c47b752ba5b502fb3955590c2f6d870ea36
Andrius <andriuss0316@gmail.com>
2021-07-11 13:10:40 +0300

    20210711

---
+++ main.js
@@ -0,0 +1,13 @@
+const allIngredientsND = document.querySelectorAll('li');
+
+const darzoves = [];
+
+for (let i = 0; i < allIngredientsND.length; i++) {
+  const item = allIngredientsND[i];
+  const itemName = item.innerText;
+  const nameInUpperCase = itemName.toUpperCase();
+  
+  darzoves.push(nameInUpperCase);
+  console.log(itemName, nameInUpperCase);
+}
+console.log(darzoves);
